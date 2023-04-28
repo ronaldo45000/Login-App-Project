138bdae587eece4ed62f1ed93904a1d725afbabe
Thinh Le <105025799+ronaldo45000@users.noreply.github.com>
2023-04-27 17:59:29 -0700

    Update Developer.java

--- model/Developer.java
+++ model/Developer.java
@@ -1,5 +1,29 @@
 package model;
 
+
 public class Developer {
-    
+
+
+	
+	
+	// name of the developers
+	String name;
+	// name of the email of developers
+	String email;
+	
+	/**
+	 * 
+	 *  This is the constructor of Developer that has name and email
+	 */
+	public Developer(String name, String email) {
+		
+		this.name = name;
+		this.email = email;
+		
+		
+		
+	}
+	//Main GUI call this class and type name and email
+	
+
 }
