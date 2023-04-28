9df1faff2490fad76fdb6b425bff0cbe9f73e135
Thinh Le <105025799+ronaldo45000@users.noreply.github.com>
2023-04-27 17:52:23 -0700

    Update Developer.java

--- model/Developer.java
+++ model/Developer.java
@@ -1,10 +1,28 @@
-package thinh;
+
 
 public class Developer {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+	
+	
+	// name of the developers
+	String name;
+	// name of the email of developers
+	String email;
+	
+	/**
+	 * 
+	 *  This is the constructor of Developer that has name and email
+	 */
+	public Developer(String name, String email) {
+		
+		this.name = name;
+		this.email = email;
+		
+		
+		
 	}
+	//Main GUI call this class and type name and email
+	
 
 }
