3d580dc43de922f6ebf689ac96d29e2858333fce
TCSS360Mauve <129917338+TCSS360Mauve@users.noreply.github.com>
2023-05-02 17:59:40 -0700

    Update AboutScreen.java

--- src/view/AboutScreen.java
+++ src/view/AboutScreen.java
@@ -1,7 +1,7 @@
 package view;
 
 import model.AppInfo;
-import model.Developer;
+import model.Account;
 import model.User;
 
 import javax.swing.*;
@@ -11,20 +11,32 @@ import java.awt.event.ActionListener;
 import java.util.Iterator;
 public class AboutScreen extends JPanel {
     
+    /** Object to store app info. */
     private final AppInfo info;
 
+    /**
+     * Constructor to create the About screen GUI.
+     *
+     * @param user The user of the app
+     * @param cardPanel The panels to swap to/from
+     * @param cardLayout The cardlayout used to swap between panels
+     */
     public AboutScreen(User user, JPanel cardPanel, CardLayout cardLayout) {
+        
         info = new AppInfo(user);
 
+        // Create basic GUI elements
         setLayout(new BorderLayout());
         JLabel aboutLabel = new JLabel("About");
         aboutLabel.setFont(new Font("Arial", Font.BOLD, 30));
         aboutLabel.setHorizontalAlignment(SwingConstants.CENTER);
         add(aboutLabel, BorderLayout.NORTH);
 
+        // GridBagLayout used to organize text
         JPanel aboutInfo = new JPanel(new GridBagLayout());
         GridBagConstraints cons = new GridBagConstraints();
 
+        // Version text
         JLabel versionLabel = new JLabel("App Version " + info.getVersion());
         versionLabel.setFont(new Font("Arial", Font.PLAIN, 20));
         cons.anchor = GridBagConstraints.SOUTH;
@@ -33,12 +45,14 @@ public class AboutScreen extends JPanel {
         cons.insets = new Insets(20, 0, 0, 0);
         aboutInfo.add(versionLabel, cons);
 
+        // User text
         JLabel userLabel = new JLabel("This app is registered to: " + user.getName() + " (" + user.getEmail() + ")");
         userLabel.setFont(new Font("Arial", Font.PLAIN, 16));
         cons.gridy = 1;
         cons.insets = new Insets(50, 0, 0, 0);
         aboutInfo.add(userLabel, cons);
 
+        // Developer label text
         JLabel devLabel = new JLabel("This app is provided by " + info.getTeamName() + ":");
         devLabel.setFont(new Font("Arial", Font.PLAIN, 16));
         devLabel.setHorizontalAlignment(SwingConstants.CENTER);
@@ -46,19 +60,21 @@ public class AboutScreen extends JPanel {
         cons.insets = new Insets(40, 0, 0, 0);
         aboutInfo.add(devLabel, cons);
 
-        Iterator<Developer> itr = info.getDevelopers().iterator();
+        // Developer list text (done in HTML)
+        Iterator<Account> itr = info.getDevelopers().iterator();
         cons.gridy = 3;
         cons.insets = new Insets(0, 0, 0, 0);
         StringBuilder str = new StringBuilder();
         str.append("<html><ul>");
         while (itr.hasNext()) {
-            Developer dev = itr.next();
+            Account dev = itr.next();
             str.append("<li>" + dev.getName() + " (" + dev.getEmail() + ")</li>");
         }
         str.append("</ul><html>");
         JLabel devs = new JLabel(str.toString());
         aboutInfo.add(devs, cons);
 
+        // Back button
         cons.gridy = 4;
         cons.weighty = 1;
         cons.insets = new Insets(0, 0, 30, 0);
