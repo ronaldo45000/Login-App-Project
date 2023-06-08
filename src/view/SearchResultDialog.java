b2035144c44df0fd0104eb635b8e6faedcd809d5
44hassan <68564400+44hassan@users.noreply.github.com>
2023-06-07 23:33:40 -0700

    Update SearchResultDialog.java
    working search

--- src/view/SearchResultDialog.java
+++ src/view/SearchResultDialog.java
@@ -29,9 +29,11 @@ public class SearchResultDialog extends JDialog {
      */
 
     public SearchResultDialog(String searchKey) {
+        // Create and format the search key label
         JLabel searchK= new JLabel("Search Key: " + searchKey);
         searchK.setFont(new Font(searchK.getFont().getName(), Font.BOLD, searchK.getFont().getSize() + 4));
         add(searchK);
+        // Retrieve all documents from the document controller
         results = DocumentController.getAllDocs();
 
 
@@ -45,28 +47,32 @@ public class SearchResultDialog extends JDialog {
         // Create the results panel and add the found results
         resultsPanel = new JPanel();
         resultsPanel.setLayout(new BoxLayout(resultsPanel, BoxLayout.Y_AXIS));
-
+        
         //Adding each document that is found.
         for (Map.Entry<String, Document>k : results.entrySet() ) {
             Document currentDoc = results.get(k.getKey());
-             JButton openButton = new JButton("Open");
-             JButton readButton = new JButton("Read");
-            readButton.addActionListener(e->{
-                new DocumentTabRead(currentDoc);
-            });
-            openButton.addActionListener(e->{
-                if(!currentDoc.openDoc()) {
-                    JOptionPane.showMessageDialog(this, "This document does not have an attached file!");
-                }
-            });
+            // Check if the documents name or description contains the search key
+            if(currentDoc.getDocumentName().contains(searchKey.toLowerCase()) || currentDoc.getDocumentDescription().contains(searchKey.toLowerCase())) {
+               JButton openButton = new JButton("Open");
+               JButton readButton = new JButton("Read");
+               // Add action listeners to the buttons
+               readButton.addActionListener(e->{
+                   new DocumentTabRead(currentDoc);
+               });
+               openButton.addActionListener(e->{
+                   if(!currentDoc.openDoc()) {
+                       JOptionPane.showMessageDialog(this, "This document does not have an attached file!");
+                   }
+               });
 
-            JPanel itemPanel = new JPanel(new FlowLayout(FlowLayout.TRAILING));
-            String resultItem = String.format("%-50s %-12s $%.2f", currentDoc.getDocumentName(), currentDoc.getDate()+"|", currentDoc.getTotalCost());
-            JLabel resultLabel = new JLabel( resultItem);
-            itemPanel.add(resultLabel);
-            itemPanel.add(openButton);
-            itemPanel.add(readButton);
-            resultsPanel.add(itemPanel);
+               JPanel itemPanel = new JPanel(new FlowLayout(FlowLayout.TRAILING));
+               String resultItem = String.format("%-50s %-12s $%.2f", currentDoc.getDocumentName(), currentDoc.getDate()+"|", currentDoc.getTotalCost());
+               JLabel resultLabel = new JLabel( resultItem);
+               itemPanel.add(resultLabel);
+               itemPanel.add(openButton);
+               itemPanel.add(readButton);
+               resultsPanel.add(itemPanel);
+            }
         }
 
         // Create a scroll pane for the results panel
@@ -81,11 +87,8 @@ public class SearchResultDialog extends JDialog {
         setContentPane(mainPanel);
         setLocationRelativeTo(SearchResultDialog.this);
         setTitle("Searching For " + "\"" + searchKey + "\"");
-        setSize(700, 500);
+        setSize(460, 500);
         setLocationRelativeTo(null);
         setVisible(true);
-
-
-
     }
 }
